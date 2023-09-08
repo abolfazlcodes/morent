1dcc5476cad5d5b78f3d2282b426549935249fa1
Abolfazl <abolfazljamshididev@gmail.com>
2023-09-08 15:25:43 +0330

    add login form handler and validations

--- src/features/authentication/LoginForm.tsx
+++ src/features/authentication/LoginForm.tsx
@@ -3,28 +3,61 @@ import Input from "../../ui/Input";
 import FormRowVertical from "../../ui/FormRowVertical";
 // import SpinnerMini from "../../ui/SpinnerMini";
 import Button from "../../ui/Button";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { useEffect } from "react";
+
+type LoginFormTypes = {
+  email: string;
+  password: string;
+};
 
 function LoginForm() {
+  const {
+    formState: { errors },
+    handleSubmit,
+    reset,
+    register,
+    setFocus,
+  } = useForm<LoginFormTypes>();
+
+  const onSubmit: SubmitHandler<LoginFormTypes> = (data) => {
+    console.log(data);
+
+    // resetting the form
+    reset();
+  };
+
+  useEffect(() => {
+    setFocus("email");
+  }, [setFocus]);
+
   return (
-    <Form type='regular'>
-      <FormRowVertical label='Email address'>
+    <Form type='regular' onSubmit={handleSubmit(onSubmit)}>
+      <FormRowVertical label='Email address' error={errors.email?.message}>
         <Input
           type='email'
           id='email'
           autoComplete='username'
-          //   disabled={isLoading}
-          //   value={email}
-          //   onChange={(e) => setEmail(e.target.value)}
+          placeholder='john@doe.com'
+          {...register("email", {
+            required: "Please enter your email",
+            pattern: {
+              value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+              message:
+                "Incorrect email format. Please enter a valid email address",
+            },
+          })}
         />
       </FormRowVertical>
-      <FormRowVertical label='Password'>
+      <FormRowVertical label='Password' error={errors.password?.message}>
         <Input
           type='password'
           id='password'
           autoComplete='current-password'
-          //   disabled={isLoading}
-          //   value={password}
-          //   onChange={(e) => setPassword(e.target.value)}
+          placeholder='Your password'
+          {...register("password", {
+            required: "Please enter your password",
+          })}
         />
       </FormRowVertical>
       <FormRowVertical>
